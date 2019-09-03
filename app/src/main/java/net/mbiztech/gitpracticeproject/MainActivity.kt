05db8c4ba38aa32c7a6748b6c75d807b0afabbbe
Md. Bashir Fardoush <bashir.pure@gmail.com>
2019-09-03 16:08:22 +0600

    add function added

--- app/src/main/java/net/mbiztech/gitpracticeproject/MainActivity.kt
+++ app/src/main/java/net/mbiztech/gitpracticeproject/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+         // simple method to test pull down
+        add(2,3);
+    }
+    
+    private fun add(a : Int, b : Int){
+
+        var c : Int  = a+b;
+
     }
 }
